f2b2ab7f0f1cb2c206020aff2738d05262f0cb53
Liam <geffc1454@gmail.com>
2025-03-29 20:56:56 +0800

    nvim: rollback to nvim 10 lsp setup

--- dot-config/nvim/lua/settings/lspconfig.lua
+++ dot-config/nvim/lua/settings/lspconfig.lua
@@ -101,13 +101,28 @@ return function()
 	-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 	capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
-	vim.lsp.config("*", {
-		capabilities = capabilities,
-		root_markers = { ".git" },
-	})
-
 	local servers = require("core.globals").lsp_servers
 	for _, server in ipairs(servers) do
-		vim.lsp.enable_server(server)
+		if server == "clangd" then
+			capabilities.offsetEncoding = { "utf-16" }
+		end
+
+		local opts = {
+			capabilities = capabilities,
+		}
+
+		-- find settings.lsp-settings/*
+		local require_ok, conf_opts = pcall(require, "services.lsp-settings." .. server)
+		if require_ok then
+			opts = vim.tbl_deep_extend("force", conf_opts, opts)
+		end
+
+		if server == "tailwindcss-language-server" then
+			require("lspconfig").tailwindcss.setup({})
+			goto continue
+		else
+			require("lspconfig")[server].setup(opts)
+		end
+		::continue::
 	end
 end
