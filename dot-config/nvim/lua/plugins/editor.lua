7c3bff8092ca5845a2605dbee326b66a8a24e93c
Liam <geffc1454@gmail.com>
2025-04-01 23:40:13 +0800

    nvim: minipair and diagnostic keymap

--- dot-config/nvim/lua/plugins/editor.lua
+++ dot-config/nvim/lua/plugins/editor.lua
@@ -28,7 +28,27 @@ return {
 	},
 
 	-- Autopairs
-	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = require("settings.autopairs") },
+	-- { "windwp/nvim-autopairs", event = "InsertEnter", opts = require("settings.autopairs") },
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		version = "*",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+		config = function(_, opts)
+			require("utils.mini").pair(opts)
+		end,
+	},
 
 	-- Comment
 	{
@@ -61,7 +81,7 @@ return {
 		opts = {},
         --stylua: ignore
 		keys = {
-			{ "<leader>st", "<CMD>Trouble todo toggle<CR>" },
+			{ "<leader>st", "<CMD>Trouble todo toggle<CR>", desc = "Todo" },
 		},
 	},
 
--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -22,7 +22,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		map("n", "ga", function() vim.lsp.buf.code_action() end, { desc = "Code Action" })
 		map("n", "gw", "<CMD>Trouble diagnostics toggle<CR>", { desc = "Show Workspace Diagnostics" })
 		map("n", "K", function() vim.lsp.buf.hover() end, { desc = "Hover Documentation" })
-		map("n", "gk", "<CMD>Lspsaga diagnostic_jump_prev<CR>", { desc = "Previous Diagnostic" })
+		map("n", "gk", function() vim.diagnostic.open_float() end, { desc = "Previous Diagnostic" })
+    map("n", "gK", function() local new_config = not vim.diagnostic.config().virtual_lines vim.diagnostic.config({ virtual_lines = new_config }) end, { desc = "Toggle diagnostic virtual_lines" })
 		map("n", "gI", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, { desc = "Toggle Inlay Hint" })
 		map("n", "gH", function() vim.cmd("checkhealth vim.lsp") end, { desc = "Toggle Inlay Hint" })
 		map("n", "gR", function() vim.lsp.stop_client(vim.lsp.get_clients()) vim.defer_fn(function() vim.cmd("edit") end, 2000) end, { desc = "Lsp Restart" })
@@ -56,7 +57,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- Diagnostics
 vim.diagnostic.config({
 	severity_sort = true,
-	float = { border = "rounded", source = "if_many" },
+	float = { border = "single", source = "always" },
 	underline = { severity = vim.diagnostic.severity.ERROR },
 	signs = {
 		text = {
@@ -67,17 +68,12 @@ vim.diagnostic.config({
 		},
 	},
 	-- virtual_text = {
-	-- 	source = "if_many",
+	-- 	source = "always",
 	-- 	prefix = "●",
 	-- 	spacing = 2,
 	-- },
 	-- virtual_lines = { current_line = true },
 })
-
-vim.keymap.set("n", "gK", function()
-	local new_config = not vim.diagnostic.config().virtual_lines
-	vim.diagnostic.config({ virtual_lines = new_config })
-end, { desc = "Toggle diagnostic virtual_lines" })
 
 return {
 	-- Services installer
@@ -113,6 +109,7 @@ return {
 		opts = {
 			library = {
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
 			},
 		},
 	},
@@ -140,14 +137,14 @@ return {
 	{ -- Autoformat
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
+		cmd = "ConformInfo",
 		keys = {
 			{
 				"<leader>f",
 				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
+					require("conform").format({ timeout_ms = 500, lsp_format = "fallback" })
 				end,
-				mode = "",
+				mode = { "n", "v" },
 				desc = "Format buffer",
 			},
 		},
@@ -158,12 +155,30 @@ return {
 	},
 	{ -- Autolint
 		"mfussenegger/nvim-lint",
-		config = function()
+		opts = {
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		},
+		config = function(_, opts)
 			require("lint").linters_by_ft = config.linting_services
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
+			print(vim.inspect(config.linting_services))
+			local M = {}
+			function M.debounce(ms, fn)
+				local timer = vim.uv.new_timer()
+				if not timer then
+					vim.notify("Failed to create timer", vim.log.levels.ERROR)
+					return fn -- Fallback to calling the function directly
+				end
+				return function(...)
+					timer:start(ms, 0, function()
+						timer:stop()
+						vim.schedule_wrap(fn)()
+					end)
+				end
+			end
+
+			vim.api.nvim_create_autocmd(opts.events, {
+				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+				callback = M.debounce(100, require("lint").try_lint),
 			})
 		end,
 	},
