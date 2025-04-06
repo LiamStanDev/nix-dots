70d6d9ac1d4eb199f0b0e1a5fe3f7e9bb5cd7c7b
Liam <geffc1454@gmail.com>
2025-04-06 13:27:38 +0800

    nvim: remove custom ui select
    Becuse we can easily set focus list on default picker setup

--- dot-config/nvim/lua/utils/picker.lua
+++ dot-config/nvim/lua/utils/picker.lua
@@ -44,31 +44,6 @@ function M.open_task_menu()
 		table.insert(entries, { label = task.label, command = task.command })
 	end
 
-	-- Snacks.picker.pick({
-	-- 	title = "Tasks",
-	-- 	items = entries,
-	-- 	source = "select",
-	-- 	layout = {
-	-- 		preview = false,
-	-- 		layout = {
-	-- 			height = math.floor(math.min(vim.o.lines * 0.8 - 10, #entries + 2) + 0.5),
-	-- 		},
-	-- 	},
-	-- 	on_show = function()
-	-- 		vim.cmd.stopinsert()
-	-- 	end,
-	-- 	actions = {
-	-- 		confirm = function(picker, item)
-	-- 			picker:close()
-	-- 			vim.schedule(function()
-	-- 				local user_cmd = vim.fn.input("command: ", item.command)
-	-- 				local dir = vim.fn.getcwd()
-	-- 				Snacks.terminal.open(user_cmd, { cwd = dir, auto_close = false })
-	-- 			end)
-	-- 		end,
-	-- 	},
-	-- })
-
 	-- use vim.ui.select
 	vim.ui.select(entries, {
 		prompt = "Select Task",
@@ -83,70 +58,5 @@ function M.open_task_menu()
 		end
 	end)
 end
-
--- ---@generic T
--- ---@param items T[] Arbitrary items
--- ---@param opts? {prompt?: string, format_item?: (fun(item: T): string), kind?: string}
--- ---@param on_choice fun(item?: T, idx?: number)
--- local function select(items, opts, on_choice)
--- 	assert(type(on_choice) == "function", "on_choice must be a function")
--- 	opts = opts or {}
---
--- 	---@type snacks.picker.finder.Item[]
--- 	local finder_items = {}
--- 	for idx, item in ipairs(items) do
--- 		local text = (opts.format_item or tostring)(item)
--- 		table.insert(finder_items, {
--- 			formatted = text,
--- 			text = idx .. " " .. text,
--- 			item = item,
--- 			idx = idx,
--- 		})
--- 	end
---
--- 	local title = opts.prompt or "Select"
--- 	title = title:gsub("^%s*", ""):gsub("[%s:]*$", "")
--- 	local completed = false
---
--- 	---@type snacks.picker.finder.Item[]
--- 	return Snacks.picker.pick({
--- 		source = "select",
--- 		items = finder_items,
--- 		format = Snacks.picker.format.ui_select(opts.kind, #items),
--- 		title = title,
--- 		layout = {
--- 			preview = false,
--- 			layout = {
--- 				height = math.floor(math.min(vim.o.lines * 0.8 - 10, #items + 2) + 0.5),
--- 			},
--- 		},
--- 		actions = {
--- 			confirm = function(picker, item)
--- 				if completed then
--- 					return
--- 				end
--- 				completed = true
--- 				picker:close()
--- 				vim.schedule(function()
--- 					on_choice(item and item.item, item and item.idx)
--- 				end)
--- 			end,
--- 		},
--- 		on_show = function()
--- 			vim.cmd.stopinsert()
--- 		end,
--- 		on_close = function()
--- 			if completed then
--- 				return
--- 			end
--- 			completed = true
--- 			vim.schedule(on_choice)
--- 		end,
--- 	})
--- end
---
--- function M.setup_ui_select()
--- 	vim.ui.select = select
--- end
 
 return M
