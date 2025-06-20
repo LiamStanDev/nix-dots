c6e0d2e02d2b3bcf0ea37cd382de6aa9d08a5064
liam <geffc1454@gmail.com>
2025-06-21 00:35:41 +0800

    chore: new vpn ssh

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -9,9 +9,24 @@
       #   port = 51322;
       #   identityFile = "~/.ssh/id_ed25519";
       # };
+
+      vivo = {
+        hostname = "100.90.207.127";
+        user = "liam";
+        port = 55022;
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
+      b660m = {
+        hostname = "100.88.200.118";
+        user = "liam";
+        port = 55022;
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
       "github.com" = {
         hostname = "github.com";
-        identityFile = "~/.ssh/git_ed25519";
+        identityFile = "~/.ssh/id_ed25519";
       };
     };
   };
