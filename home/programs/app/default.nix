75abb9013aa070122e5b8b66af552a88c427fec7
Liam Lin <zhonglin0107@gmail.com>
2025-06-29 12:28:31 +0800

    2025-06-29 12:28:31

--- home/programs/app/default.nix
+++ home/programs/app/default.nix
@@ -16,23 +16,24 @@
   # Connect to other devices
   services.kdeconnect.enable = true;
 
+  # Note: I install app not only here but also flatpak. (comments is use flatpak)
   home.packages = with pkgs; [
-    google-chrome # Chromium-based web browser
-    telegram-desktop # Messaging app
+    # google-chrome # Chromium-based web browser
+    # telegram-desktop # Messaging app
     obsidian # Markdown-based note-taking app
-    pgadmin4-desktopmode # PostgreSQL GUI management
-    spotify # Music streaming app
-    postman # API testing tool
+    # pgadmin4-desktopmode # PostgreSQL GUI management
+    # spotify # Music streaming app
+    # postman # API testing tool
     bitwarden-desktop # Password manager
     pavucontrol # PulseAudio volume control GUI
     nautilus # GNOME file manager
     file-roller # GNOME compression manager (GUI)
-    discord # Online text and voice chat
+    # discord # Online text and voice chat
     gedit # text editor GUI for Gnome
     coolercontrol.coolercontrol-gui # fancontrol GUI
     coolercontrol.coolercontrol-ui-data # fancontrol UI data
-    libsForQt5.okular # PDF reader
-    gimp3 # Image editor
+    # libsForQt5.okular # PDF reader
+    # gimp3 # Image editor
     loupe # Image viewer (GTK)
     (bottles.override {removeWarningPopup = true;}) # Windows software using Wine
   ];
