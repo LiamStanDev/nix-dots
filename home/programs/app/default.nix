bc364c2af11b95bbed51bf01cbe264cbfd092360
Liam Lin <zhonglin0107@gmail.com>
2025-06-29 21:39:12 +0800

    2025-06-29 21:39:12

--- home/programs/app/default.nix
+++ home/programs/app/default.nix
@@ -21,22 +21,14 @@
 
   # Note: I install app not only here but also flatpak. (comments is use flatpak)
   home.packages = with pkgs; [
-    # google-chrome # Chromium-based web browser
-    # telegram-desktop # Messaging app
     obsidian # Markdown-based note-taking app
-    # pgadmin4-desktopmode # PostgreSQL GUI management
-    # spotify # Music streaming app
-    # postman # API testing tool
     bitwarden-desktop # Password manager
     pavucontrol # PulseAudio volume control GUI
     nautilus # GNOME file manager
     file-roller # GNOME compression manager (GUI)
-    # discord # Online text and voice chat
     gedit # text editor GUI for Gnome
     coolercontrol.coolercontrol-gui # fancontrol GUI
     coolercontrol.coolercontrol-ui-data # fancontrol UI data
-    # libsForQt5.okular # PDF reader
-    # gimp3 # Image editor
     loupe # Image viewer (GTK)
   ];
 }
