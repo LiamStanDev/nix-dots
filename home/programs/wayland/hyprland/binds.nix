681b20b6964416c171977d6ab575cb65b4493394
Liam Lin <zhonglin0107@gmail.com>
2025-07-03 20:55:53 +0800

    2025-07-03 20:55:53

--- home/programs/wayland/hyprland/binds.nix
+++ home/programs/wayland/hyprland/binds.nix
@@ -66,7 +66,7 @@ in {
 
     # Launch apps
     # "$mainMod, RETURN, exec, [float;tile] ${pkgs.wezterm}/bin/wezterm start --always-new-process"
-    "$mainMod, RETURN, exec, [tile] ${pkgs.ghostty}/bin/ghostty -e tmux"
+    "$mainMod, RETURN, exec, [tile] ghostty -e tmux"
     "$mainMod, SPACE, exec, pkill rofi || exec ~/.config/rofi/scripts/launcher_t6"
     "$mainMod, E, exec, ${pkgs.nautilus}/bin/nautilus"
     "$mainMod, T, exec, pkill foot || ${pkgs.foot}/bin/foot --title=btop -e btop"
